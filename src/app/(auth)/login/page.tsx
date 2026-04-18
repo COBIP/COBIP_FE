1cbbdfc9f0beaf7c20cd58eda6279440dd66ff3d
EndDefault <sungyu0127@gmail.com>
2026-04-18 16:54:27 +0900

    ✨ Feat: 로그인 페이지 생성
    app/auth/login/page.tsx생성

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,29 @@
+// import React from 'react';
+
+const LoginPage = () => {
+    const meshBackgroundStyle = {
+        backgroundColor: "#f6f6f8",
+        backgroundImage: `
+        radial-gradient(at 0% 0%, rgba(215, 201, 251, 0.4) 0px, transparent 50%),
+        radial-gradient(at 100% 100%, rgba(215, 201, 251, 0.3) 0px, transparent 50%),
+        radial-gradient(at 50% 50%, rgba(215, 201, 251, 0.1) 0px, transparent 50%)
+        `,
+    };
+    
+    return(
+        <div className="flex w-full min-h-screen bg-white">
+            <div
+                className="hidden lg:flex lg:w-1/2 items-center justify-center relative overflow-hidden"
+                style={meshBackgroundStyle}
+            >
+                {/* <LeftPage /> */}
+            </div>
+
+            <div className="w-full lg:w-1/2 flex items-center justify-center p-8">
+                {/* <LoginForm /> */}
+            </div>
+        </div>
+    );
+}
+
+export default LoginPage;
