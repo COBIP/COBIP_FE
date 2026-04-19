98476e472bee859d32d54355dafb0900274dbdc3
EndDefault <sungyu0127@gmail.com>
2026-04-19 11:22:17 +0900

    ✨  feat: signup/page.tsx생성
    로그인에서 사용한 LeftPage.tsx를 사용(좌측 히어로 섹션)

---
+++ src/app/(auth)/signup/page.tsx
@@ -0,0 +1,29 @@
+
+import LeftPage from "@/features/auth/components/LeftPage";
+
+
+export default function SignupPage() {
+    const meshBackgroundStyle = {
+        backgroundColor: "#f6f6f8",
+        backgroundImage: `
+        radial-gradient(at 0% 0%, rgba(215, 201, 251, 0.4) 0px, transparent 50%),
+        radial-gradient(at 100% 100%, rgba(215, 201, 251, 0.3) 0px, transparent 50%),
+        radial-gradient(at 50% 50%, rgba(215, 201, 251, 0.1) 0px, transparent 50%)
+        `,
+    };
+
+    return(
+        <div className="flex w-full min-h-screen bg-white">
+            <div
+                className="hidden lg:flex lg:w-1/2 items-center justify-center relative overflow-hidden"
+                style={meshBackgroundStyle}
+            >
+                <LeftPage />
+            </div>
+
+            <div className="w-full lg:w-1/2 flex items-center justify-center p-8">
+                
+            </div>
+        </div>
+    );
+}
