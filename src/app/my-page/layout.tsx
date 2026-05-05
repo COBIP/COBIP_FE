1673c066d0c9bca3be967650ee9e26a34efc9265
EndDefault <sungyu0127@gmail.com>
2026-05-05 17:10:30 +0900

    ✨  feat: 마이페이지 레이아웃 및 프로필 라우팅 구조 세팅
    layout.tsx 생성, my-page/profile/page.tsx 생성

---
+++ src/app/my-page/layout.tsx
@@ -0,0 +1,15 @@
+import MyPageHeader from "@/features/my-page/components/MyPageHeader"; 
+import MyPageSidebar from "@/features/my-page/components/MyPageSidebar";
+
+export default function MyPageLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex min-h-screen">
+        <MyPageHeader />
+        <MyPageSidebar />
+    
+        <div className="flex flex-col flex-1 min-w-0 pt-16 md:pl-64">
+            <main className="p-4 md:p-8">{children}</main>
+        </div>
+    </div>
+  );
+}
---
+++ src/app/my-page/profile/page.tsx
@@ -0,0 +1,7 @@
+export default function MyPageProfile() {
+    return(
+        <div>
+
+        </div>
+    );
+}
