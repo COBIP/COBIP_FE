a82967930c98bd66fca03f86d018f30e91e9a097
EndDefault <sungyu0127@gmail.com>
2026-05-05 23:15:58 +0900

    💄  style: 창모드 배경색 잘림 해결 및 콘텐츠 높이 상속 구조 수정

--- src/app/my-page/layout.tsx
+++ src/app/my-page/layout.tsx
@@ -3,12 +3,12 @@ import MyPageSidebar from "@/features/my-page/components/common/MyPageSidebar";
 
 export default function MyPageLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex min-h-screen">
-        <MyPageHeader />
+    <div className="flex min-h-screen bg-[#fcf9f8]">
+          <MyPageHeader />
         <MyPageSidebar />
     
         <div className="flex flex-col flex-1 min-w-0 pt-16 md:pl-64">
-            <main className="p-4 md:p-8">{children}</main>
+            <main className="flex flex-col flex-1 p-4 md:p-8">{children}</main>
         </div>
     </div>
   );
