16c34a9cd4c177582fb3a5d49ce2c24b9b306724
EndDefault <sungyu0127@gmail.com>
2026-05-05 23:15:08 +0900

    ✨  feat: 마이페이지 구성 요소 조립 및 레이아웃 최적화
    ExternalAssets, ProfileHeader, ProfileTabs 배치

--- src/app/my-page/profile/page.tsx
+++ src/app/my-page/profile/page.tsx
@@ -1,7 +1,27 @@
-export default function MyPageProfile() {
-    return(
-        <div>
+import { ExternalAssets } from '@/features/my-page/components/profile/ExternalAssets';
+import ProfileHeader from '@/features/my-page/components/profile/ProfileHeader';
+import ProfileTabs  from '@/features/my-page/components/profile/ProfileTabs';
 
-        </div>
+export default function MyPage() {
+    const user = {
+        nickName : "nickName",
+        image: "img"
+    };
+
+    return (
+        <>
+            <ExternalAssets />
+            <div className="flex flex-col flex-1 w-full h-full max-w-5xl gap-6 mx-auto">
+                <ProfileHeader 
+                    nickName={user.nickName} 
+                    profileImage={user.image} 
+                />
+                
+                <div className="flex flex-col flex-1 min-h-0">
+                    <ProfileTabs />
+                </div> 
+            </div>
+        </>
     );
 }
+
