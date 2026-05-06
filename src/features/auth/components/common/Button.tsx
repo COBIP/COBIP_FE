3ea3e8925ebc1cf1225313eb216e72575dc70a59
ksg2401 <ksg2401@g.yju.ac.kr>
2026-05-06 10:32:06 +0900

    💄  style: Input, Button 컴포넌트 Props 확장 및 타입 안정성 강화

--- src/features/auth/components/common/Button.tsx
+++ src/features/auth/components/common/Button.tsx
@@ -1,12 +1,13 @@
 interface Props {
     text: string;
+    type?: "button" | "submit" | "reset";
 }
 
 export default function Button(props: Props){
     return (
         <button
             className="mt-4 h-12 bg-[#D7C9FB] text-slate-900 font-bold rounded-xl shadow-sm hover:shadow-md hover:bg-[#cfc1f3] transition-all duration-200 flex items-center justify-center"
-            type="button">
+            type={props.type || "button"}>
                 {props.text}
         </button>
     );
