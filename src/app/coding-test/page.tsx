8824e7a55e246efd58b08c702caf134b5dff5384
EndDefault <sungyu0127@gmail.com>
2026-05-08 19:20:26 +0900

    🔥  remove: page에 메인페이지 header삭제
    공용 header를 사용할 수 있기 때문에 삭제

--- src/app/coding-test/page.tsx
+++ src/app/coding-test/page.tsx
@@ -4,7 +4,6 @@ import { useState } from 'react';
 import { ProblemGrid } from '@/features/coding-test/components/ProblemGrid';
 import CodingTestFilter from '@/features/coding-test/components/CodingTestFilter';
 import { useCodingTest } from '@/hooks/useCodingTest';
-import { Header } from '@/features/main-home/components/Header';
 
 // 한 페이지에 보여줄 문제 개수 상수 선언
 const ITEMS_PER_PAGE = 16;
@@ -50,7 +49,6 @@ export default function CodingTestPage() {
 
     return (
         <div className="bg-white min-h-screen pb-20">
-            <Header />
             <main className="flex-grow max-w-[1440px] mx-auto w-full px-8 py-10">
                 <CodingTestFilter 
                     selectedFilters={filters} 
