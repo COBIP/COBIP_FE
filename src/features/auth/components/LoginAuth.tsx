06294efda6128fd262758fdb26b2a7908109cc0e
EndDefault <sungyu0127@gmail.com>
2026-05-09 19:59:01 +0900

    ✨  feat: 로그인 API 연동 및 실제 로직 적용
    임시 데이터를 제거하고 실제 백엔드 로그인 API와 Zustand 상태 관리를 연결

--- src/features/auth/components/LoginAuth.tsx
+++ src/features/auth/components/LoginAuth.tsx
@@ -2,29 +2,37 @@
 
 import { useState } from "react";
 import { useRouter } from "next/navigation"; // ✨ 2. 페이지 이동을 위한 useRouter (next/router 아님 주의!)
+import { useAuthMutation } from "@/hooks/useAuthMutation"; //
 
 export default function LoginAuth() {
-    const router = useRouter();
+    const { mutateLogin, isLoading } = useAuthMutation(); //
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
-    const handleLogin = (e: React.FormEvent) => {
-        e.preventDefault(); // form의 기본 기능인 '페이지 새로고침'을 막아줍니다.
+    // const handleLogin = (e: React.FormEvent) => {
+    //     e.preventDefault(); // form의 기본 기능인 '페이지 새로고침'을 막아줍니다.
 
-        // ✨ 3. 더미 데이터로 로그인 검사
-        if (email === "test@cobip.com" && password === "1234") {
-            // 로그인 성공!
-            alert("로그인 성공! 환영합니다.");
+    //     // ✨ 3. 더미 데이터로 로그인 검사
+    //     if (email === "test@cobip.com" && password === "1234") {
+    //         // 로그인 성공!
+    //         alert("로그인 성공! 환영합니다.");
             
-            // 메인 페이지로 이동
-            router.push("/main-home"); 
+    //         // 메인 페이지로 이동
+    //         router.push("/main-home"); 
             
-            /* (나중에는 여기에 상태 관리 라이브러리(Zustand 등)나 
-               localStorage를 써서 '나 로그인했음' 이라는 증표를 남겨둘 겁니다) */
-        } else {
-            // 로그인 실패!
-            alert("이메일이나 비밀번호를 다시 확인해주세요.");
-        }
+    //         /* (나중에는 여기에 상태 관리 라이브러리(Zustand 등)나 
+    //            localStorage를 써서 '나 로그인했음' 이라는 증표를 남겨둘 겁니다) */
+    //     } else {
+    //         // 로그인 실패!
+    //         alert("이메일이나 비밀번호를 다시 확인해주세요.");
+    //     }
+    // };
+
+    const handleLogin = async (e: React.FormEvent) => {
+        e.preventDefault();
+        
+        // 1. 서버로 정보를 보냅니다. (이메일, 비밀번호)
+        await mutateLogin({ email, password }); // 
     };
 
     return (
