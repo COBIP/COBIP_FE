5a5e30aa26105f19e88aa85fe0a60776be742559
EndDefault <sungyu0127@gmail.com>
2026-05-10 18:38:26 +0900

    🐛  fix: 마이페이지 header 더미 데이터와 변수 삭제
    유저 더미데이터 삭제

--- src/app/my-page/profile/page.tsx
+++ src/app/my-page/profile/page.tsx
@@ -2,18 +2,11 @@ import ProfileHeader from '@/features/my-page/components/profile/ProfileHeader';
 import ProfileTabs  from '@/features/my-page/components/profile/ProfileTabs';
 
 export default function MyPage() {
-    const user = {
-        nickName : "nickName",
-        image: "/test.png"
-    };
 
     return (
         <>
             <div className="flex flex-col flex-1 w-full h-full max-w-5xl gap-6 mx-auto">
-                <ProfileHeader 
-                    nickName={user.nickName} 
-                    profileImage={user.image} 
-                />
+                <ProfileHeader />
                 
             
                 <div className="flex flex-col flex-1 min-h-0">
