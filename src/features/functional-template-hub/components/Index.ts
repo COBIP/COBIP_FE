7de1257e3d09cd980a7459818f415c456407e3fe
minseo-00 <limminsoe08@gmail.com>
2026-05-13 09:27:57 +0900

    ✨ feat: AI 템플릿 생성 허브 연결

--- src/features/functional-template-hub/components/Index.ts
+++ src/features/functional-template-hub/components/Index.ts
@@ -1,4 +1,5 @@
 export { AISection } from '@/features/functional-template-hub/components/AISection';
+export { AiGeneratedTemplatePreview } from '@/features/functional-template-hub/components/AiGeneratedTemplatePreview';
 export { TemplateGrid } from '@/features/functional-template-hub/components/TemplateGrid';
 export { TemplateCard } from '@/features/functional-template-hub/components/TemplateCard';
 export { LoadingState } from '@/features/functional-template-hub/components/LoadingState';
