5b049c5cb7f051152d385b01259c1b7da5bff7a3
EndDefault <sungyu0127@gmail.com>
2026-06-22 13:50:15 +0900

    🔥  remove: 로그인 페이지 로고 이미지 삭제
    로그인 페이지 로고 옆 이미지 삭제

--- src/features/auth/components/LeftPage.tsx
+++ src/features/auth/components/LeftPage.tsx
@@ -5,12 +5,6 @@ export default function LoginHero() {
         <div>
             {/* ✨ 2. 최상단 div를 Link로 변경하고 href="/" 추가 */}
             <Link href="/main-home" className="absolute top-12 left-12 flex items-center gap-3 cursor-pointer hover:opacity-80 transition">
-                <div className="w-8 h-8 bg-[#D7C9FB] rounded-lg flex items-center justify-center text-slate-900 shadow-sm">
-                    <svg className="w-5 h-5" fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                        <rect fill="currentColor" height="16" rx="3" width="16" x="4" y="4"></rect>
-                        <circle cx="12" cy="12" fill="#f6f6f8" r="3"></circle>
-                    </svg>
-                </div>
                 <h2 className="text-slate-900 text-2xl font-bold tracking-tight">COBIP</h2>
             </Link>
 
